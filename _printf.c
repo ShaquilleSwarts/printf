0b072997e25e7712058036c37135eeee7a762608
Shaquille Swarts <ShaquilleSwarts12@gmail.com>
2022-07-12 07:13:54 -0700

    betty update

--- _printf.c
+++ _printf.c
@@ -2,27 +2,27 @@
 #include <stdarg.h>
 #include <stddef.h>
 /**
- * _printf - receives specifiers 
+ * _printf - receives specifiers
  * @format: Given string
  * Return: characters printed
  */
 
 int _printf(const char *format, ...)
 {
-  int printed_chars;
-  conver_t f_list[] = {
-		      {"c", char_format},
-		      {"s", string_format},
-		      {"%", percent_format},
-		      {NULL, NULL}
-  };
-  va_list arg_list;
+int printed_chars;
+conver_t f_list[] = {
+{"c", char_format},
+{"s", string_format},
+{"%", percent_format},
+{NULL, NULL}
+};
+va_list arg_list;
 
-  if (format == NULL)
-    return (-1);
+if (format == NULL)
+return (-1);
 
-  va_start(arg_list, format);
-  printed_chars = reader(format, f_list, arg_list);
-  va_end(arg_list);
-  return (printed_chars);
+va_start(arg_list, format);
+printed_chars = reader(format, f_list, arg_list);
+va_end(arg_list);
+return (printed_chars);
 }
